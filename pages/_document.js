068f1e76e90e9e63ee487e6b7ae9b8bf1f94203e
Joe Pea <trusktr@gmail.com>
2018-09-05 16:21:47 -0700

    show how to manually add a sheet to the registry

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,14 @@ export default class AppDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheets = new SheetsRegistry();
 
+    sheets.add(jss.createStyleSheet({
+        '@global': {
+            '*': {
+                border: '1px solid red' // everything should be boxed in red
+            }
+        }
+    }))
+
     const decoratePage = Page => props =>
       <JssProvider registry={sheets}>
         <Page {...props} />
